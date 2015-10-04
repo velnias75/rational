bab5cfadedae53880e9c91d1a513e18fff477134
Heiko Schaefer <heiko@rangun.de>
2015-10-04 20:04:39 +0200

    fix for gmp >= 5.1

--- src/test/gmptest.cpp
+++ src/test/gmptest.cpp
@@ -25,35 +25,37 @@ using namespace Commons::Math;
 
 GMPTest::GMPTest() : CppUnit::TestFixture() {}
 
-void GMPTest::setUp() {
-    m_sqrt2 = unchecked_sqrt ( mpf_class ( std::sqrt ( 2.0 ) ) );
+void GMPTest::setUp()
+{
+     m_sqrt2 = unchecked_sqrt ( mpf_class ( std::sqrt ( 2.0 ) ) );
 }
 
 void GMPTest::tearDown() {}
 
-void GMPTest::testConstruct() {
+void GMPTest::testConstruct()
+{
 
 #ifdef __EXCEPTIONS
-    CPPUNIT_ASSERT_THROW ( Rational<rational_type> r ( 1, 0 ), std::domain_error );
+     CPPUNIT_ASSERT_THROW ( Rational<rational_type> r ( 1, 0 ), std::domain_error );
 #endif
 
-    CPPUNIT_ASSERT_EQUAL (
-        std::string ( "6933950472684408894045574639154274115485852426842518240159" ),
-        m_sqrt2.numerator().get_str() );
+     CPPUNIT_ASSERT_EQUAL (
+          std::string ( "6933950472684408894045574639154274115485852426842518240159" ),
+          m_sqrt2.numerator().get_str() );
 
-    CPPUNIT_ASSERT_EQUAL (
-        std::string ( "4903043399646811761765323314534746386982093618212104008044" ),
-        m_sqrt2.denominator().get_str() );
+     CPPUNIT_ASSERT_EQUAL (
+          std::string ( "4903043399646811761765323314534746386982093618212104008044" ),
+          m_sqrt2.denominator().get_str() );
 
-    const mpf_class a = Rational<rational_type> ( 1, 2 );
-    const mpf_class b = Rational<rational_type> ( 1, -2 );
-    const mpf_class c = Rational<rational_type> ( -1, 2 );
-    const mpf_class d = Rational<rational_type> ( -1, -2 );
+     const mpf_class a = Rational<rational_type> ( 1, 2 );
+     const mpf_class b = Rational<rational_type> ( 1, -2 );
+     const mpf_class c = Rational<rational_type> ( -1, 2 );
+     const mpf_class d = Rational<rational_type> ( -1, -2 );
 
-    CPPUNIT_ASSERT_EQUAL ( 0.5, a.get_d() );
-    CPPUNIT_ASSERT_EQUAL ( -0.5, b.get_d() );
-    CPPUNIT_ASSERT_EQUAL ( -0.5, c.get_d() );
-    CPPUNIT_ASSERT_EQUAL ( 0.5, d.get_d() );
+     CPPUNIT_ASSERT_EQUAL ( 0.5, a.get_d() );
+     CPPUNIT_ASSERT_EQUAL ( -0.5, b.get_d() );
+     CPPUNIT_ASSERT_EQUAL ( -0.5, c.get_d() );
+     CPPUNIT_ASSERT_EQUAL ( 0.5, d.get_d() );
 }
 
-// kate: indent-mode cstyle; indent-width 4; replace-tabs on; 
+// kate: indent-mode cstyle; indent-width 5; replace-tabs on; 
